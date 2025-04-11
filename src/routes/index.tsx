431f42e4f9e824e6a5e99fb297b2c831f723f2e8
Eralp Karaduman <eralp.karaduman@starship.co>
2025-04-11 19:24:10 +0300

    separate wheel material

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,28 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 import '../App.css'
-import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { Model } from '../components/E-model'
-import { CameraControls, Environment } from '@react-three/drei'
-import { useRef } from 'react'
-import { Group } from 'three'
+import { CameraControls, Environment,  ContactShadows } from '@react-three/drei'
 
-function Lights() {
-  const groupRef = useRef<Group>(null)
-  const { camera } = useThree()
-  
-  useFrame(() => {
-    if (groupRef.current) {
-      groupRef.current.position.copy(camera.position)
-    }
-  })
-
-  return (
-    <group ref={groupRef}>
-      <directionalLight position={[10, 10, 5]} intensity={1.5} castShadow />
-      <directionalLight position={[-5, 5, -5]} intensity={0.5} castShadow />
-    </group>
-  )
-}
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -34,18 +15,37 @@ function App() {
       <Canvas 
         style={{ height: '100vh', width: '100vw' }}
         camera={{ position: [20, 10, 20], fov: 50 }}
+        shadows
       >
-        <color attach="background" args={['#000000']} />
-        <ambientLight intensity={0.3} />
         <Environment 
-          preset="sunset"
+          preset="warehouse"
           background
-          blur={1}
+          blur={2}
+          backgroundIntensity={0.1}
           resolution={256}
         />
-        <Model position={[0, -4, 0]} />
-        <CameraControls makeDefault />
-        <Lights />
+        <ContactShadows
+          position={[0, -3, 0]}
+          opacity={0.8}
+          scale={20}
+          blur={4}
+          far={50.5}
+          resolution={256}
+          color="#000000"
+        />
+        <Model position={[0, -3, 0]} scale={1} />
+        <CameraControls 
+          makeDefault
+          minPolarAngle={0} 
+          maxPolarAngle={1.65}
+          minDistance={10}
+          maxDistance={200}
+          minAzimuthAngle={-Infinity}
+          maxAzimuthAngle={Infinity}
+        >
+          <directionalLight position={[10, 10, 5]} intensity={0.2} castShadow />
+          <directionalLight position={[-5, 5, -5]} intensity={0.2} castShadow />
+        </CameraControls>
       </Canvas>
     </div>
   )
