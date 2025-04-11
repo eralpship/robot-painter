93e71fe5b24bce130713bf8c8097f5f52a972c8c
Eralp Karaduman <eralp.karaduman@starship.co>
2025-04-11 12:32:58 +0300

    import model

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router'
-import logo from '../logo.svg'
 import '../App.css'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Model } from '../components/E-model'
+import { CameraControls, Environment } from '@react-three/drei'
+import { useRef } from 'react'
+import { Group } from 'three'
+
+function Lights() {
+  const groupRef = useRef<Group>(null)
+  const { camera } = useThree()
+  
+  useFrame(() => {
+    if (groupRef.current) {
+      groupRef.current.position.copy(camera.position)
+    }
+  })
+
+  return (
+    <group ref={groupRef}>
+      <directionalLight position={[10, 10, 5]} intensity={1.5} castShadow />
+      <directionalLight position={[-5, 5, -5]} intensity={0.5} castShadow />
+    </group>
+  )
+}
 
 export const Route = createFileRoute('/')({
   component: App,
@@ -9,28 +31,22 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="App-link"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+      <Canvas 
+        style={{ height: '100vh', width: '100vw' }}
+        camera={{ position: [20, 10, 20], fov: 50 }}
+      >
+        <color attach="background" args={['#000000']} />
+        <ambientLight intensity={0.3} />
+        <Environment 
+          preset="sunset"
+          background
+          blur={1}
+          resolution={256}
+        />
+        <Model position={[0, -4, 0]} />
+        <CameraControls makeDefault />
+        <Lights />
+      </Canvas>
     </div>
   )
 }
