150a979a6601bb0d18d099fdc9132b9d181d9c97
Eralp Karaduman <eralp.karaduman@starship.co>
2025-08-16 19:11:08 +0300

    remove fiziks

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import '../App.css'
 import { Canvas } from '@react-three/fiber'
 import { Model, type ModelRef } from '../components/E-model'
 import { OrbitControls, ContactShadows, Environment } from '@react-three/drei'
-import { Physics, usePlane } from '@react-three/cannon'
 import type { OrbitControls as OrbitControlsImpl } from 'three-stdlib'
 import { useRef, useState, useEffect, Suspense } from 'react'
 import { useThree } from '@react-three/fiber'
@@ -25,30 +24,15 @@ export const Route = createFileRoute('/')({
 
 function CameraController({ fov }: { fov: number }) {
   const { camera } = useThree()
-  
+
   useEffect(() => {
     if ('fov' in camera) {
       camera.fov = fov
       camera.updateProjectionMatrix()
     }
   }, [camera, fov])
-  
-  return null
-}
 
-function Ground() {
-  const [ref] = usePlane(() => ({
-    rotation: [-Math.PI / 2, 0, 0], // Horizontal orientation
-    position: [0, -3, 0], // Match robot ground level
-    type: 'Static'
-  }))
-  
-  return (
-    <mesh ref={ref}>
-      <planeGeometry args={[10, 15]} />
-      <meshBasicMaterial color="yellow" wireframe={true} />
-    </mesh>
-  )
+  return null
 }
 
 function AppContent() {
@@ -59,34 +43,86 @@ function AppContent() {
   const initialBaseColor = '#ffffff'
   const modelRef = useRef<ModelRef>(null)
 
-  const [{ baseColor, tailLightColor, headlightsOn, taillightsOn, headlightsIntensity, taillightsIntensity, lidOpen, autoRotate, ambientLight, backgroundIntensity, backgroundBlur, environmentIntensity, fov }, setControlStates] = useControls(() => ({
-      baseColor: {
-        value: initialBaseColor,
-        label: 'Base Color',
-      },
-      tailLightColor: {
-        value: '#ff0000',
-        label: 'Tail Light Color'
-      },
-      headlightsOn: { value: true, label: 'Headlights On' },
-      headlightsIntensity: { value: 12, label: 'Headlights Intensity', max: 60, min: 0, step: 0.1  },
-      taillightsOn: { value: true, label: 'Taillights On' },
-      taillightsIntensity: { value: 12, label: 'Taillights Intensity', max: 60, min: 0, step: 0.1  },
-      lidOpen: { value: false, label: 'Lid Open' },
-      autoRotate: { value: true, label: 'Auto Rotate' },
-      ambientLight: { value: 0.8, label: 'Ambient Light', max: 2, min: 0, step: 0.1  },
-      backgroundIntensity: { value: 0.4, label: 'Background Intensity', max: 1, min: 0, step: 0.01  },
-      backgroundBlur: { value: 0.7, label: 'Background Blur', max: 1, min: 0, step: 0.01  },
-      environmentIntensity: { value: 0.7, label: 'Environment Intensity', max: 1, min: 0, step: 0.01  },
-      fov: { value: 20, label: 'FOV', min: 5, max: 60, step: 0.5 },
-      resetCamera: button(() => controlsRef.current?.reset()),
-      touchFlag: button(() => modelRef.current?.touchFlag()),
+  const [
+    {
+      baseColor,
+      tailLightColor,
+      headlightsOn,
+      taillightsOn,
+      headlightsIntensity,
+      taillightsIntensity,
+      lidOpen,
+      autoRotate,
+      ambientLight,
+      backgroundIntensity,
+      backgroundBlur,
+      environmentIntensity,
+      fov,
+    },
+    setControlStates,
+  ] = useControls(() => ({
+    baseColor: {
+      value: initialBaseColor,
+      label: 'Base Color',
+    },
+    tailLightColor: {
+      value: '#ff0000',
+      label: 'Tail Light Color',
+    },
+    headlightsOn: { value: true, label: 'Headlights On' },
+    headlightsIntensity: {
+      value: 12,
+      label: 'Headlights Intensity',
+      max: 60,
+      min: 0,
+      step: 0.1,
+    },
+    taillightsOn: { value: true, label: 'Taillights On' },
+    taillightsIntensity: {
+      value: 12,
+      label: 'Taillights Intensity',
+      max: 60,
+      min: 0,
+      step: 0.1,
+    },
+    lidOpen: { value: false, label: 'Lid Open' },
+    autoRotate: { value: true, label: 'Auto Rotate' },
+    ambientLight: {
+      value: 0.8,
+      label: 'Ambient Light',
+      max: 2,
+      min: 0,
+      step: 0.1,
+    },
+    backgroundIntensity: {
+      value: 0.4,
+      label: 'Background Intensity',
+      max: 1,
+      min: 0,
+      step: 0.01,
+    },
+    backgroundBlur: {
+      value: 0.7,
+      label: 'Background Blur',
+      max: 1,
+      min: 0,
+      step: 0.01,
+    },
+    environmentIntensity: {
+      value: 0.7,
+      label: 'Environment Intensity',
+      max: 1,
+      min: 0,
+      step: 0.01,
+    },
+    fov: { value: 20, label: 'FOV', min: 5, max: 60, step: 0.5 },
+    resetCamera: button(() => controlsRef.current?.reset()),
+    touchFlag: button(() => modelRef.current?.touchFlag()),
   }))
 
   useEffect(() => {
     modelRef.current?.updateBaseColor(baseColor)
   }, [baseColor])
-
 
   // Functions to manipulate light states directly in Leva
   const toggleHeadlights = () => {
@@ -116,7 +152,10 @@ function AppContent() {
 
     const checkInactivity = () => {
       const now = Date.now()
-      if (hasInteracted && now - lastInteractionTime.current > inactivityTimeout) {
+      if (
+        hasInteracted &&
+        now - lastInteractionTime.current > inactivityTimeout
+      ) {
         setHasInteracted(false)
       }
       frameId = requestAnimationFrame(checkInactivity)
@@ -128,69 +167,70 @@ function AppContent() {
 
   return (
     <div className="App">
-      <Leva  theme={customLevaTheme} collapsed={false} titleBar={{ title: 'Options', filter: false }} />
+      <Leva
+        theme={customLevaTheme}
+        collapsed={false}
+        titleBar={{ title: 'Options', filter: false }}
+      />
       <Canvas
         style={{ height: '100vh', width: '100vw' }}
         camera={{ position: [40, 30, 40], fov }}
       >
-        <Physics gravity={[0, -9.81, 0]} size={10} allowSleep={false}>
-          <Ground />
-          <CameraController fov={fov} />
-          <Suspense fallback={null}>
-            <Environment 
-              preset="dawn"
-              background
-              blur={backgroundBlur}
-              backgroundIntensity={backgroundIntensity}
-              environmentIntensity={environmentIntensity}
-              resolution={256}
-            />
-          </Suspense>
-
-          {/* Ambient light to control overall brightness */}
-          <ambientLight intensity={ambientLight} />
-
-          {/* Simple contact shadow */}
-          <ContactShadows 
-            position={[0, -2.9, 0]}
-            opacity={2.5}
-            scale={180}
-            blur={2}
-            far={100}
+        <CameraController fov={fov} />
+        <Suspense fallback={null}>
+          <Environment
+            preset="dawn"
+            background
+            blur={backgroundBlur}
+            backgroundIntensity={backgroundIntensity}
+            environmentIntensity={environmentIntensity}
             resolution={256}
-            color="#000000"
           />
-          <Model 
-            ref={modelRef}
-            position={[0, -3, 0]} 
-            scale={1} 
-            tailLightColor={tailLightColor}
-            headlightsOn={headlightsOn}
-            taillightsOn={taillightsOn}
-            onToggleHeadlights={toggleHeadlights}
-            onToggleTaillights={toggleTaillights}
-            lidOpen={lidOpen}
-            setLidOpen={lidOpen => setControlStates({ lidOpen })}
-            initialBaseColor={initialBaseColor}
-            headlightsIntensity={headlightsIntensity}
-            taillightsIntensity={taillightsIntensity}
-          />
-          <OrbitControls 
-            ref={controlsRef}
-            makeDefault
-            minPolarAngle={0} 
-            maxPolarAngle={1.55}
-            minDistance={10}
-            maxDistance={200}
-            minAzimuthAngle={-Infinity}
-            maxAzimuthAngle={Infinity}
-            autoRotate={!hasInteracted && autoRotate}
-            autoRotateSpeed={2}
-            onStart={handleInteraction}
-          />
-        </Physics>
+        </Suspense>
+
+        {/* Ambient light to control overall brightness */}
+        <ambientLight intensity={ambientLight} />
+
+        {/* Simple contact shadow */}
+        <ContactShadows
+          position={[0, -2.9, 0]}
+          opacity={2.5}
+          scale={180}
+          blur={2}
+          far={100}
+          resolution={256}
+          color="#000000"
+        />
+        <Model
+          ref={modelRef}
+          position={[0, -3, 0]}
+          scale={1}
+          tailLightColor={tailLightColor}
+          headlightsOn={headlightsOn}
+          taillightsOn={taillightsOn}
+          onToggleHeadlights={toggleHeadlights}
+          onToggleTaillights={toggleTaillights}
+          lidOpen={lidOpen}
+          setLidOpen={lidOpen => setControlStates({ lidOpen })}
+          initialBaseColor={initialBaseColor}
+          headlightsIntensity={headlightsIntensity}
+          taillightsIntensity={taillightsIntensity}
+        />
+        <OrbitControls
+          ref={controlsRef}
+          makeDefault
+          minPolarAngle={0}
+          maxPolarAngle={1.55}
+          minDistance={10}
+          maxDistance={200}
+          minAzimuthAngle={-Infinity}
+          maxAzimuthAngle={Infinity}
+          autoRotate={!hasInteracted && autoRotate}
+          autoRotateSpeed={2}
+          onStart={handleInteraction}
+        />
       </Canvas>
-      <FloatingCollapsibleWindow title='Texture Editor'>
+      <FloatingCollapsibleWindow title="Texture Editor">
         <TextureEditorWrapper baseColor={baseColor} />
       </FloatingCollapsibleWindow>
     </div>
